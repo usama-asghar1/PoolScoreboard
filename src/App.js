361aa94916f6d743fe22706d4de3f38d3a031172
usama-asghar1 <usama.asghar@hotmail.co.uk>
2023-07-10 13:05:32 +0100

    another backend table added

--- src/App.js
+++ src/App.js
@@ -31,20 +31,25 @@ function App() {
   
   const generateMatches = () => {
     const matches = [];
-
+  
     // Iterate over each person
     for (let i = 0; i < tableData.length; i++) {
       const person1 = tableData[i].Name;
-
+  
       // Iterate over other persons
       for (let j = i + 1; j < tableData.length; j++) {
         const person2 = tableData[j].Name;
         const match = `${person1} vs ${person2}`;
-        matches.push(match);
+  
+        // Exclude the finished matches
+        if (!finishedMatches.includes(match)) {
+          matches.push(match);
+        }
       }
     }
     return matches;
   };
+  
 
   
     // State to track the selected winner for each match
@@ -57,6 +62,7 @@ function App() {
         [match]: winner
       }));
     };
+    
   
     const updateWins = async (person) => {
       try {
@@ -88,15 +94,109 @@ function App() {
       }
     };
 
+    const updateLosses = async (person) => {
+      try {
+        const { data, error } = await supabase
+          .from('Leaderboard')
+          .select('Losses')
+          .eq('Name', person)
+          .single();
+    
+        if (error) {
+          console.error('Error fetching losses:', error);
+          return;
+        }
+    
+        const updatedLosses = data.Losses + 1;
+    
+        const { error: updateError } = await supabase
+          .from('Leaderboard')
+          .update({ Losses: updatedLosses })
+          .eq('Name', person);
+    
+        if (updateError) {
+          console.error('Error updating losses:', updateError);
+        } else {
+          console.log(`Successfully updated losses for ${person}`);
+        }
+      } catch (error) {
+        console.error('Error updating losses:', error);
+      }
+    };
+
+    const [finishedMatches, setFinishedMatches] = useState([]);
+    const [matchHistory, setMatchHistory] = useState([]);
+    
+
+    const fetchMatchHistory = () => {
+      return supabase
+        .from('MatchHistory')
+        .select('*')
+        .order('id', { ascending: false }) // Order by id column in descending order
+        .then(({ data, error }) => {
+          if (error) {
+            console.error('Error fetching match history:', error);
+            throw error;
+          } else {
+            setMatchHistory(data);
+            return data; // Return the fetched data
+          }
+        })
+        .catch((error) => {
+          console.error('Error fetching match history:', error);
+          throw error;
+        });
+    };
+    
+    useEffect(() => {
+      fetchMatchHistory();
+    }, []);
+    
+    
+    
+    
+    
+
     const handleSubmit = async (match) => {
+      const [person1, person2] = match.split(' vs ');
       const winner = winners[match];
+    
       if (winner) {
-        await updateWins(winner);
+        const loser = winner === person1 ? person2 : person1;
+        
+        await Promise.all([updateWins(winner), updateLosses(loser)]);
         await fetchTableData(); // Fetch the updated table data
+       // Fetch the updated match history
+    
+       setFinishedMatches(prevState => [...prevState, match]);
+       setWinners(prevState => ({ ...prevState, [match]: winner }));
+     
+       await fetchMatchHistory(); // Fetch the updated match history
+        try {
+          const { data, error } = await supabase
+            .from('MatchHistory')
+            .insert([{ match, winner }]);
+            console.log(data);
+      
+          if (error) {
+            console.error('Error storing match result:', error);
+          } else {
+            console.log('Match result stored successfully.');
+          }
+        } catch (error) {
+          console.error('Error storing match result:', error);
+        }
+    
+        setFinishedMatches(prevState => [...prevState, match]);
+        setWinners(prevState => ({ ...prevState, [match]: winner }));
+        await fetchMatchHistory();
       } else {
         console.error('No winner selected for match:', match);
       }
     };
+    
+    
+    
     
     const handleAddPlayer = async () => {
       const playerName = prompt("Enter the name of the new player:");
@@ -120,8 +220,26 @@ function App() {
         }
       }
     };
-    
 
+    const handleDeletePlayer = async (player) => {
+      try {
+        const { error } = await supabase
+          .from('Leaderboard')
+          .delete()
+          .eq('Name', player.Name);
+    
+        if (error) {
+          console.error('Error deleting player:', error);
+        } else {
+          console.log(`Player ${player.Name} deleted successfully.`);
+          fetchTableData(); // Fetch the updated table data
+        }
+      } catch (error) {
+        console.error('Error deleting player:', error);
+      }
+    };
+  
+    
   return (
     <div className="App">
       <h1>Pool Score Board</h1>
@@ -135,14 +253,20 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {tableData.map((row, index) => (
-            <tr key={index}>
-              <td>{row.Name}</td>
-              <td>{row.Wins + row.Losses}</td>
-              <td>{row.Wins}</td>
-              <td>{row.Losses}</td>
-            </tr>
-          ))}
+        {tableData.map((row, index) => (
+  <tr key={index}>
+    <td>{row.Name}</td>
+    <td>{row.Wins + row.Losses}</td>
+    <td>{row.Wins}</td>
+    <td>{row.Losses}</td>
+    <td>
+      <button className="delete-button" onClick={() => handleDeletePlayer(row)}>
+        Delete ❌
+      </button>
+    </td>
+  </tr>
+))}
+
         </tbody>
       </table>
       
@@ -181,6 +305,20 @@ function App() {
         ))}
       </ul>
     </div>
+
+   
+    <h2>Previous Matches</h2>
+{matchHistory.length > 0 && (
+  <ul>
+    {matchHistory.map((match, index) => (
+      <li key={index}>
+        <span>{match.match}</span>
+        <span> Winner: {match.winner}</span>
+      </li>
+    ))}
+  </ul>
+)}
+
 
     </div>
   );
